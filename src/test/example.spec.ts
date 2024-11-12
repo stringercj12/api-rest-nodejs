8f8b3dbcf14a34ef447fa878ee810e1a5b723b6e
Jefferson Ferreira <jefferson.ferreira@mjv.com.br>
2024-11-12 10:37:03 -0300

    Testes automatizados - Criando primeiro teste

---
+++ src/test/example.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+
+test('o usuário consegue criar uma nova transação', () => {
+    // fazer a chamada HTTP p/ criar uma nova transação
+
+
+    const responseStatusCode = 201;
+
+    expect(responseStatusCode).toEqual(201)
+})
